2899e305a9737df5811dc7ff1f9611f3db5cb820
Sierra MacLeod <sierra.m.macleod@gmail.com>
2019-10-16 19:43:41 -0600

    main page edits

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -35,15 +35,19 @@ import borealisGroup from '../images/borealisGroup.jpg'
 import horizon from '../images/horizon.jpg'
 import spacegrant from '../images/msgc.png'
 import jump from '../images/jump.jpg'
+import borealisAcronym from '../images/borealisAcronym.png'
 import Row from 'react-bootstrap/Row'
 import Column from 'react-bootstrap/Col'
-import Parallax from './Parallax'
+import Card from 'react-bootstrap/Card'
+import SimpleParallax from './Parallax'
+import { Parallax, Background } from 'react-parallax';
+import {Parallax as SpringParallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
 export default class MainPage extends Component {
   render () {
     return (
       <div id={'main-page'}>
-        <Parallax image={borealisGroup} height={'90vh'}>
+        {/*<SimpleParallax image={borealisGroup} height={'90vh'}>
           <Row className={'h-100 justify-content-center mx-0'}>
             <Column xs={12} sm={12} md={8} lg={6} xl={6} className={'my-auto'}>
               <Jumbotron className={'mx-0'} style={{backgroundColor: 'rgba(255, 255, 255, 0.90)'}}>
@@ -57,6 +61,28 @@ export default class MainPage extends Component {
               </Jumbotron>
             </Column>
           </Row>
+        </SimpleParallax>*/}
+        <Parallax
+          blur={0}
+          bgImage={borealisGroup}
+          bgImageAlt='Borealis 2019 Interns'
+          strength={500}
+        >
+          <div style={{ height: '90vh' }}>
+            <Row className={'h-100 justify-content-center mx-0'}>
+              <Column xs={12} sm={12} md={8} lg={6} xl={6} className={'my-auto'}>
+                <Jumbotron className={'mx-0'} style={{backgroundColor: 'rgba(255, 255, 255, 0.90)'}}>
+                  <h1>Time to Fly!</h1>
+                  <p>Check out the new Borealis Flight Tracker</p>
+                  <p>
+                    <LinkContainer to={'/tracking'}>
+                      <Button variant={'primary'} href={'/tracking'}>Start Tracking</Button>
+                    </LinkContainer>
+                  </p>
+                </Jumbotron>
+              </Column>
+            </Row>
+          </div>
         </Parallax>
         <Container>
           <Row className={'justify-content-center my-3'}>
@@ -70,7 +96,7 @@ export default class MainPage extends Component {
             </Column>
           </Row>
         </Container>
-        <Parallax image={horizon} height={'90vh'} minHeight={'500px'}>
+        {/*<SimpleParallax image={horizon} height={'90vh'} minHeight={'500px'}>
           <Container fluid style={{paddingTop: '5vh'}}>
             <Row>
               <Column sm={12} md={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
@@ -79,44 +105,47 @@ export default class MainPage extends Component {
               </Column>
             </Row>
           </Container>
+        </SimpleParallax>*/}
+        <Parallax
+          bgImage={horizon}
+          bgImageAlt="Earth from 100,000 feet - 2019 Borealis Flight"
+          strength={-200}
+        >
+          <div style={{ height: '90vh', minHeight: '500px'}}>
+            <Container style={{paddingTop: '5vh'}}>
+              <h2 className={'text-white display-2'}>Reaching New Heights</h2>
+              <h2 className={'text-white'}>One of the most cost-effective high-altitude testing platforms around</h2>
+            </Container>
+          </div>
         </Parallax>
-        <Container fluid>
+        <Container fluid className={'text-center'}>
+          <h3 className={'display-3 my-2'}>Who are we?</h3>
+        </Container>
+        <Container className={'pt-2'}>
           <Row>
-            <Column sm={12} md={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
-              <h3 className={'display-3'}>Who are we?</h3>
+            <Column sm={12} md={8} lg={6}>
+              <Card border={'info'} className={'my-2 mx-2'}>
+                <Card.Body style={{minHeight: '60vh'}}>
+                  <Image src={borealisAcronym} alt={'BOREALIS Acronym'} fluid className={'py-2'}/>
+                  <Card.Text className={'py-2'}>
+                    BOREALIS is MSGC's high altitude ballooning program. In the program students from a variety of curricula
+                    work together to conceive, design and build payloads that are flown up to 100,000 feet - the edge of space.
+                    BOREALIS has two complete ballooning programs at Montana State University and the University of Montana.
+                    Smaller affiliate campuses develop science and engineering payloads to fly in coordination with the two
+                    university programs.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Column>
+            <Column sm={12} md={8} lg={6}>
+              <Card border={'info'} className={'my-2 mx-2'}>
+                <Card.Img variant={"top"} src={jump}/>
+              </Card>
             </Column>
           </Row>
         </Container>
-        <Parallax image={jump} height={'30vh'} minHeight={'400px'} position={'right'} size={'auto 80vh'}>
-          <Container fluid style={{paddingTop: '5vh'}}>
-            <Row>
-              <Column sm={12} md={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
-                <p style={{width: '30vw'}}>
-                  <strong>B</strong>alloon <strong>O</strong>utreach, <strong>R</strong>esearch, <strong>E</strong>xploration
-                  and <strong>L</strong>andscape <strong>I</strong>maging <strong>S</strong>ystem
-                </p>
-                <p style={{width: '30vw'}}>
-                  BOREALIS is MSGC's high altitude ballooning program. In the program students from a variety of curricula
-                  work together to conceive, design and build payloads that are flown up to 100,000 feet - the edge of space.
-                  BOREALIS has two complete ballooning programs at Montana State University and the University of Montana.
-                  Smaller affiliate campuses develop science and engineering payloads to fly in coordination with the two
-                  university programs.
-                </p>
-              </Column>
-            </Row>
-          </Container>
-        </Parallax>
-        <Container fluid>
-          <Row>
-            <Column sm={12} md={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
-              <h3 className={'display-3 text-white'}>Some more text</h3>
-            </Column>
-          </Row>
-          <Row>
-            <Column sm={12} md={{span: 10, offset: 1}} lg={{span: 8, offset: 2}}>
-              <h3 className={'display-3 text-white'}>And more text</h3>
-            </Column>
-          </Row>
+        <Container>
+          <div style={{height: '200px'}}/>
         </Container>
       </div>
     )
