a3ef71a8427b994cc8e8dfe0581f1d2195bdc7ae
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-04-14 22:27:28 -0700

    flight sel: change badge to button

--- src/components/FlightSelect.jsx
+++ src/components/FlightSelect.jsx
@@ -8,7 +8,7 @@ import Form from 'react-bootstrap/Form'
 import Tab from 'react-bootstrap/Tab'
 import Nav from 'react-bootstrap/Nav'
 import Dropdown from 'react-bootstrap/Dropdown';
-import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
 
 
 export default class FlightSelect extends Component {
@@ -104,16 +104,27 @@ export default class FlightSelect extends Component {
                 {this.state.selectedOrgFilter &&
                   <p>
                     Filtered by
-                    <Badge variant={"primary"} onClick={() => {this.setState({selectedOrgFilter: null})}}>
-                      {this.state.selectedOrgFilter} ✖
-                    </Badge>
+                    <Button
+                      variant={"primary"}
+                      onClick={() => {
+                        this.setState({selectedOrgFilter: null})
+                      }}
+                      size={'sm'}
+                    >
+                      {this.state.selectedOrgFilter}
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           className="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path
+                          d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                      </svg>
+                    </Button>
                   </p>
                 }
               </Column>
               {/* Button for filter by organization */}
               <Column className={'pl-0'}>
-                <Dropdown drop={'left'} variant={'light'}>
-                  <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                <Dropdown drop={'left'}>
+                  <Dropdown.Toggle variant="light" id="dropdown-basic">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-funnel" viewBox="0 0 16 16">
                       <path
