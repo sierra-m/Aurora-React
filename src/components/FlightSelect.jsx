a3a67e08f08d3664ddc7add8170cf8b5d57a08bb
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-04-14 22:38:59 -0700

    flight sel: fix filter button, filter align

--- src/components/FlightSelect.jsx
+++ src/components/FlightSelect.jsx
@@ -102,8 +102,10 @@ export default class FlightSelect extends Component {
                   autoFocus={true}
                 />
                 {this.state.selectedOrgFilter &&
-                  <p>
-                    Filtered by
+                  <div className={'pt-1'}>
+                    <p>
+                      <small><em>>Filtered by:</em></small>
+                    </p>
                     <Button
                       variant={"primary"}
                       onClick={() => {
@@ -113,17 +115,17 @@ export default class FlightSelect extends Component {
                     >
                       {this.state.selectedOrgFilter}
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                           className="bi bi-x-lg" viewBox="0 0 16 16">
+                           className="bi bi-x" viewBox="0 0 16 16">
                         <path
-                          d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                       </svg>
                     </Button>
-                  </p>
+                  </div>
                 }
               </Column>
               {/* Button for filter by organization */}
               <Column className={'pl-0'}>
-                <Dropdown drop={'left'}>
+                <Dropdown menuAlign="right">
                   <Dropdown.Toggle variant="light" id="dropdown-basic">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-funnel" viewBox="0 0 16 16">
@@ -133,6 +135,7 @@ export default class FlightSelect extends Component {
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
+                    <h6>Filter by organization</h6>
                     <Select
                       value={this.state.selectedOrgOption}
                       onChange={this.orgSelectChange}
