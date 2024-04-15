4ba4747cb712b2891a41661aa67800b9898d5e7d
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-04-14 23:00:32 -0700

    flight sel: fix link align, filter color

--- src/components/FlightSelect.jsx
+++ src/components/FlightSelect.jsx
@@ -113,9 +113,7 @@ export default class FlightSelect extends Component {
                       >
                         {this.state.selectedOrgFilter}
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             className="bi bi-x-square" viewBox="0 0 16 16">
-                          <path
-                            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                             className="bi bi-x pl-1" viewBox="0 0 16 16">
                           <path
                             d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                         </svg>
@@ -128,8 +126,8 @@ export default class FlightSelect extends Component {
               {/* Button for filter by organization */}
               <Column className={'pl-0'}>
                 <Dropdown alignRight={true}>
-                  <Dropdown.Toggle variant="light" id="dropdown-basic">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-funnel" viewBox="0 0 16 16">
                       <path
                         d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
