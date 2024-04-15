a6062985cf0af3d1b924ad38901d47e655f15e55
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-04-14 23:16:00 -0700

    flight sel: improve link wrapping, fix filter column

--- src/components/FlightSelect.jsx
+++ src/components/FlightSelect.jsx
@@ -104,7 +104,10 @@ export default class FlightSelect extends Component {
                 {this.state.selectedOrgFilter &&
                   <div className={'pt-1'}>
                     <p>
-                      <small><em>Filtered by:</em> <a
+                      <small><em>Filtered by: </em></small>
+                    </p>
+                    <p>
+                      <a
                         className={'text-primary link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover'}
                         href={"#"}
                         onClick={() => {
@@ -117,17 +120,16 @@ export default class FlightSelect extends Component {
                           <path
                             d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                         </svg>
-                      </a></small>
+                      </a>
                     </p>
-
                   </div>
                 }
               </Column>
               {/* Button for filter by organization */}
-              <Column className={'pl-0'}>
+              <Column xs={3} className={'pl-0'}>
                 <Dropdown alignRight={true}>
                   <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-funnel" viewBox="0 0 16 16">
                       <path
                         d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
