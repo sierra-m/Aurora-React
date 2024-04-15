a5ea081720048613e632da78d434bae122ee3051
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-04-14 22:45:04 -0700

    flight sel: change filter clear to a link

--- src/components/FlightSelect.jsx
+++ src/components/FlightSelect.jsx
@@ -104,22 +104,23 @@ export default class FlightSelect extends Component {
                 {this.state.selectedOrgFilter &&
                   <div className={'pt-1'}>
                     <p>
-                      <small><em>>Filtered by:</em></small>
+                      <small><em>Filtered by:</em></small>
                     </p>
-                    <Button
-                      variant={"primary"}
+                    <a
+                      className={'text-primary link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover'}
                       onClick={() => {
                         this.setState({selectedOrgFilter: null})
                       }}
-                      size={'sm'}
                     >
-                      {this.state.selectedOrgFilter}
+                      {this.state.selectedOrgFilter} 
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                           className="bi bi-x" viewBox="0 0 16 16">
+                           className="bi bi-x-square" viewBox="0 0 16 16">
+                        <path
+                          d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
                         <path
                           d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                       </svg>
-                    </Button>
+                    </a>
                   </div>
                 }
               </Column>
@@ -129,7 +130,7 @@ export default class FlightSelect extends Component {
                   <Dropdown.Toggle variant="light" id="dropdown-basic">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-funnel" viewBox="0 0 16 16">
-                      <path
+                    <path
                         d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
                     </svg>
                   </Dropdown.Toggle>
